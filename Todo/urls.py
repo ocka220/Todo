4517f64dc310bddbf1169d74900ba64c3d9fe74d
ocka220 <ocka@mail.ru>
2020-04-15 16:25:59 +0300

    итог

--- Todo/urls.py
+++ Todo/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from main_views import main_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_view),
 ]
