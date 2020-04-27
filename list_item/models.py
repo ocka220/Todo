92f57530747991c033c9ba62d3bffacac3751d1c
ocka220 <63516479+ocka220@users.noreply.github.com>
2020-04-27 11:55:38 +0300

    Update models.py

--- list_item/models.py
+++ list_item/models.py
@@ -6,7 +6,7 @@ class ListItemModel(models.Model):
     name = models.CharField(max_length=128, verbose_name='Название списка')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    listmodel_id = models.ForeignKey('main.ListModel', on_delete=models.CASCADE, verbose_name='Список дел')
+    listmodel = models.ForeignKey('main.ListModel', on_delete=models.CASCADE, verbose_name='Список дел')
     is_done = models.BooleanField(default=False)
     expare_data = models.DateField(blank=True, null=True)
     priority = models.SmallIntegerField(verbose_name='Приоритет', default=0)
